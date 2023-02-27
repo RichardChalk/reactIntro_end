1787beadbf59973469f974cd84d727be558ad660
Richard Chalk <barmybrit@gmail.com>
2023-02-27 16:32:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ClassComponent from "./Components/ClassComponent";
+import { FunctionComponent } from "./Components/FunctionComponent";
+
+function App() {
+  return (
+    <div>
+      <ClassComponent />
+      <FunctionComponent />
+    </div>
+  );
+}
+
+export default App;
