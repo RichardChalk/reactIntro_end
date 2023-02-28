d7a267d46ccb6e9f9d818156b925e85cbb2d117c
Richard Chalk <barmybrit@gmail.com>
2023-02-28 12:58:57 +0100

    incl Tont Stark v2

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Person = (props) => {
   return (
     <div className="person">
-      <h1>Name: {props.name}</h1>
+      <h1 className="personName">Name: {props.name}</h1>
       <h2>Surname: {props.surName}</h2>
       <h3>Age: {props.age}</h3>
     </div>
