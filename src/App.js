c80e6afa30232a11c861fde6e2ce67bcd3e10821
Richard Chalk <barmybrit@gmail.com>
2023-02-28 13:31:20 +0100

    Added counter useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ClassComponent from "./Components/ClassComponent";
+import { Counter } from "./Components/Counter";
 import { FunctionComponent } from "./Components/FunctionComponent";
 import { Person } from "./Components/Person";
 
@@ -12,6 +13,7 @@ function App() {
         <Person name={"Linda"} surName={"Chalk"} age={"47"} />
         <Person name={"Tony"} surName={"Stark"} age={"48"} />
       </div>
+      <Counter />
     </div>
   );
 }
