4364d369d6c4579fc054001d8e9e2a83f38ca0a2
Richard Chalk <barmybrit@gmail.com>
2023-02-28 11:59:27 +0100

    incl props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import ClassComponent from "./Components/ClassComponent";
 import { FunctionComponent } from "./Components/FunctionComponent";
+import { Person } from "./Components/Person";
 
 function App() {
   return (
     <div>
       <ClassComponent />
       <FunctionComponent />
+      <Person name={"Richard"} surName={"Chalk"} age={"52"} />
+      <Person name={"Linda"} surName={"Chalk"} age={"47"} />
     </div>
   );
 }
